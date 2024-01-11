ef0315479fc1a9071159daf568da633f57344072
Hooooong99 <jeongsw0428@naver.com>
2024-01-11 16:10:34 +0900

    이것은 첫번째pwm 프로젝트임

---
+++ Core/Inc/uart.h
@@ -0,0 +1,16 @@
+/*
+ * uart.h
+ *
+ *  Created on: Jan 10, 2024
+ *      Author: jeong
+ */
+
+#ifndef INC_UART_H_
+#define INC_UART_H_
+
+#include "main.h"
+
+//변수 prototype define
+void  initUart(UART_HandleTypeDef *huart);//Uart equipment reset / huart skip possible / type important
+
+#endif /* INC_UART_H_ */
